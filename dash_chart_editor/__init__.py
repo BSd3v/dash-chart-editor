8a3617433b3971b0118e2716bd10b1b58ec33b41
michaelbabyn <41019918+michaelbabyn@users.noreply.github.com>
2023-04-06 17:29:35 -0400

    remove reference to dce async chunk

--- dash_chart_editor/__init__.py
+++ dash_chart_editor/__init__.py
@@ -29,7 +29,7 @@ _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
 _this_module = _sys.modules[__name__]
 
-async_resources = ["DashChartEditor",]
+async_resources = []
 
 _js_dist = []
 
